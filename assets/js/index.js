8c0b439aec2a10cc178336163e21d6f98e3d36e5
Nick Whyte <nick@nickwhyte.com>
2018-05-28 20:30:22 +1000

    Done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        block: 'start',
-        inline: 'nearest',
-        behavior: 'smooth'
-      });
+      var href = this.getAttribute('href');
+      var targetEl = document.querySelector(href);
+      if (targetEl) {
+        e.preventDefault();
+        targetEl.scrollIntoView({
+          block: 'start',
+          inline: 'nearest',
+          behavior: 'smooth'
+        });
+        window.history.pushState(null, '', href);
+      }
     });
   });
 });
